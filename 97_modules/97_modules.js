fed4f74a56673ec8eb5d8f4ad0762235e424035e
Abdullah Saeed <85762594+AbdullahSaeed1211@users.noreply.github.com>
2023-02-20 15:00:37 +0530

    Add files via upload

---
+++ 97_modules/97_modules.js
@@ -0,0 +1,6 @@
+// const {hello,Ahello} = require("./module1")
+import zoro, {hello,Ahello} from "./module2.js";
+hello()
+Ahello('Luffy')
+Ahello('Zoro')
+zoro()
---
+++ 97_modules/module2.js
@@ -0,0 +1,15 @@
+// ES6 Modules
+export const hello = ()=>{
+    console.log("Hello Harry")
+}
+
+export const Ahello = (name)=>{
+    console.log("Hello " + name)
+}
+
+const zoro = ()=>{
+    console.log("Hello " + "Zoro")
+}
+
+export default zoro;
+ 
